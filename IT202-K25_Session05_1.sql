411ab96850ccb49a94466ced2bcc7e453c3f6da0
Nguyễn Hoàng Anh <143367993+dfrgtyhujio@users.noreply.github.com>
2026-04-20 22:22:41 +0700

    Add files via upload

--- IT202-K25_Session05_1.sql
+++ IT202-K25_Session05_1.sql
@@ -1,6 +1,20 @@
 -- Do quy tắc ưu tiên toán tử: and được ưu tiên chạy trước or
 -- Hiện tại máy hiểu là: (Quận 1) hoặc (Quận 3 và rating > 4.0)
 
+use db_demo;
+
+create table Restaurants (
+    restaurant_name varchar(50),
+    address varchar(100),
+    district varchar(20),
+    rating decimal(2,1)
+);
+
+insert into Restaurants values
+	('Phở Hòa', 'Pasteur', 'Quận 3', 4.5),
+	('Bún Bò', 'Nguyễn Du', 'Quận 1', 4.2),
+	('Cơm Tấm', 'Lê Thánh Tôn', 'Quận 1', 3.9);
+
 select restaurant_name, address, rating
 from Restaurants
 where (district = 'Quận 1' or district = 'Quận 3') and rating > 4.0;
